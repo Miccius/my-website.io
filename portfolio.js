2953c3737af613c026e47cee6131da93225c1422
Francesco Miccio <mifr82@gmail.comgit config --global user.name Miccius>
2024-09-16 09:21:06 +0100

    last update

--- portfolio.js
+++ portfolio.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Sample portfolio items (you can replace this with your own data)
     const portfolioItems = [
-        { category: 'web', image: 'Images/3.png', title: 'Web Project 1', description: 'Web design project' },
-        { category: 'brand', image: 'Images/3.png', title: 'Brand Project 1', description: 'Branding project' },
-        { category: 'ui-ux', image: 'Images/3.png', title: 'UI/UX Project 1', description: 'User interface design' },
-        { category: 'photo', image: 'Images/3.png', title: 'Photo Project 1', description: 'Photography project' },
+        { category: 'wordPress-theme', image: 'Images/3.png', title: 'Fictional-University', link: '<a  class="link-projects" href="https://github.com/Miccius" target= "_blank">Visit My Project</a>', description: 'I developed a custom WordPress site using PHP for dynamic fields and a unique theme, showcasing my skills in WordPress development and creating user-friendly solutions.' },
+        { category: 'landing page', image: 'Images/nike landing page.png', title: 'Brand Project 1', link: '<a class="link-projects" href="https://miccius.github.io/Landing-page-Nike.io/" target= "_blank" ><p>Visit My Project</p></a>', description: 'Branding project' },
+        { category: 'e-shop', image: 'Images/3.png', title: 'Brand Project 1', link: '<a class="link-projects" href="https://www.linkedin.com/in/francesco-miccio-20a1109b/" target= "_blank" ><p>Visit My Project</p></a>', description: 'Branding project' },
+       // { category: 'ui-ux', image: 'Images/3.png', title: 'UI/UX Project 1', link: '<a class="link-projects" href="https://www.linkedin.com/in/francesco-miccio-20a1109b/" target= "_blank" ><p>Visit My Project</p></a>', description: 'User interface design' },
+        //{ category: 'photo', image: 'Images/3.png', title: 'Photo Project 1', link: '<a class="link-projects" href="https://www.linkedin.com/in/francesco-miccio-20a1109b/" target= "_blank" ><p>Visit My Project</p></a>', description: 'Photography project' },
         // Add more items as needed
     ];
 
@@ -20,6 +21,8 @@ document.addEventListener('DOMContentLoaded', function() {
             <div class="portfolio-info">
                 <h3>${item.title}</h3>
                 <p>${item.description}</p>
+                <h4>${item.link}</h4>
+                
             </div>
         `;
         return div;
